1d0f85881619b5f696988dfd494db69dc83be3f1
Rafael Godoy <rafael.godoy@fotop.com.br>
2021-01-26 17:44:43 -0300

    env: configurando variáveis de ambiente

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -2,20 +2,25 @@ import { Sequelize, DataTypes, Model } from "sequelize";
 
 const Op = Sequelize.Op;
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    timezone: "-03:00",
-    dialect: process.env.DB_DIALECT,
-    pool: {
-        max: 10,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-    },
-    define: {
-        timestamps: false,
-        freezeTableName: true,
-    },
-});
+const sequelize = new Sequelize(
+    process.env.POSTGRES_DATABASE,
+    process.env.POSTGRES_USER,
+    process.env.POSTGRES_PASSWORD,
+    {
+        host: process.env.POSTGRES_SERVER,
+        timezone: "-03:00",
+        dialect: process.env.POSTGRES_DIALECT,
+        pool: {
+            max: 10,
+            min: 0,
+            acquire: 30000,
+            idle: 10000,
+        },
+        define: {
+            timestamps: false,
+            freezeTableName: true,
+        },
+    }
+);
 
 export { sequelize, Model, DataTypes, Op };
