fc64a14cb61da5d22a4eaba57bf341cb7fa8b473
Rafael Godoy <rafael.godoy@fotop.com.br>
2021-01-26 19:02:51 -0300

    env: configurações de ambiente

---
+++ src/config/migrations.js
@@ -0,0 +1,23 @@
+module.exports = {
+    development: {
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        host: process.env.POSTGRES_SERVER,
+        dialect: "postgres",
+    },
+    test: {
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        host: process.env.POSTGRES_SERVER,
+        dialect: "postgres",
+    },
+    production: {
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        host: process.env.POSTGRES_SERVER,
+        dialect: "postgres",
+    },
+};
