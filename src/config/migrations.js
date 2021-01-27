e6d1fd2d5b5bd653ef2c0307651945e28b2d271e
Rafael Godoy <rafael.godoy@fotop.com.br>
2021-01-27 12:33:17 -0300

    ci: configurando o ambiente

--- src/config/migrations.js
+++ src/config/migrations.js
@@ -16,8 +16,8 @@ module.exports = {
     production: {
         username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DATABASE,
-        host: process.env.POSTGRES_SERVER,
+        database: "dasa-api-personal",
+        host: "127.0.0.1",
         dialect: "postgres",
         ssl: true,
         dialectOptions: {
